48b8d527f1273fcfc4f5e55111a90874bb09fa8c
sirsandmann <florian@sander-net.de>
2016-12-07 00:15:51 +0100

    Added time measurement for queries

--- src/main/java/de/hsh/inform/dbp_project_readdata/TestQueries.java
+++ src/main/java/de/hsh/inform/dbp_project_readdata/TestQueries.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
@@ -14,9 +15,16 @@ public class TestQueries {
 
 	public static void main(String[] args) {
 		try (Jedis jedis = pool.getResource()) {
-			//
+			// for measuring time
+			long startTime = System.nanoTime();
+
 			//activeConnections(jedis, "898691696", "898692070");
 			hostsConntectedTo(jedis, "209143215163");
+
+			long stopTime = System.nanoTime();
+			System.out.println("\n-------------- Duration ---------------------");
+			System.out.println(                
+					TimeUnit.MILLISECONDS.convert(stopTime - startTime, TimeUnit.NANOSECONDS) + " Milliseconds");
 		}
 	}
 
@@ -42,55 +50,53 @@ public class TestQueries {
 		Set<String> resultCountersSource = new HashSet<String>();
 		Set<String> resultsCountersAll = new HashSet<String>();
 		Set<String> results = new HashSet<String>();
-		
+
 		groupKeys.add(jedis.zrangeByScore("index:" + App.sourceAddr, ip, ip));
 		groupKeys.add(jedis.zrangeByScore("index:" + App.sourcePort, port, port));
 		groupKeys.add(jedis.zrangeByScore("index:" + App.destinationAddr, ip, ip));
 		groupKeys.add(jedis.zrangeByScore("index:" + App.destinationPort, port, port));
-		
-		//Source get all IPS that are in the port and addr of source
-		resultCountersSource.addAll(
-				jedis.lrange(groupKeys.get(0).toString().replace(",", "")  //remove the commas
-				    .replace("[", "")  //remove the right bracket
-				    .replace("]", ""), 0, -1 )
-		);
-		resultCountersSource.retainAll(
-				jedis.lrange(groupKeys.get(1).toString().replace(",", "")  //remove the commas
-				    .replace("[", "")  //remove the right bracket
-				    .replace("]", ""), 0, -1 )
-		);
-		
-		//Destiation get all IPS that are in the port and addr of source
-		resultCountersDestination.addAll(
-				jedis.lrange(groupKeys.get(2).toString().replace(",", "")  //remove the commas
-				    .replace("[", "")  //remove the right bracket
-				    .replace("]", ""), 0, -1 )
-		);
-		resultCountersDestination.retainAll(
-				jedis.lrange(groupKeys.get(3).toString().replace(",", "")  //remove the commas
-				    .replace("[", "")  //remove the right bracket
-				    .replace("]", ""), 0, -1 )
-		);
-		
-		//Add Source and Destination to a single set -->remove automatically duplicates
+
+		// Source get all IPS that are in the port and addr of source
+		resultCountersSource.addAll(jedis.lrange(groupKeys.get(0).toString().replace(",", "") // remove
+																								// the
+																								// commas
+				.replace("[", "") // remove the right bracket
+				.replace("]", ""), 0, -1));
+		resultCountersSource.retainAll(jedis.lrange(groupKeys.get(1).toString().replace(",", "") // remove
+																									// the
+																									// commas
+				.replace("[", "") // remove the right bracket
+				.replace("]", ""), 0, -1));
+
+		// Destiation get all IPS that are in the port and addr of source
+		resultCountersDestination.addAll(jedis.lrange(groupKeys.get(2).toString().replace(",", "") // remove
+																									// the
+																									// commas
+				.replace("[", "") // remove the right bracket
+				.replace("]", ""), 0, -1));
+		resultCountersDestination.retainAll(jedis.lrange(groupKeys.get(3).toString().replace(",", "") // remove
+																										// the
+																										// commas
+				.replace("[", "") // remove the right bracket
+				.replace("]", ""), 0, -1));
+
+		// Add Source and Destination to a single set -->remove automatically
+		// duplicates
 		resultsCountersAll.addAll(resultCountersDestination);
 		resultsCountersAll.addAll(resultCountersSource);
-		
-		System.out.println(resultsCountersAll);
-		
+
 		// get the source and address values
 		for (String s : resultsCountersAll) {
 			String src = jedis.hget("meta:" + s, App.sourceAddr);
 			String dest = jedis.hget("meta:" + s, App.destinationAddr);
-			
-			//remove all non numeric values
-			if(!src.replaceAll("\\D+", "").equals(ip)){
+
+			// remove all non numeric values
+			if (!src.replaceAll("\\D+", "").equals(ip)) {
 				results.add(src);
-			}else{
+			} else {
 				results.add(dest);
 			}
 		}
-		
 		System.out.println(results);
 
 	}
